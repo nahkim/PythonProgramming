456a10ef935bcd2c19d8726e32f1516b3179f16a
hyunn2 <48798620+hyunn2@users.noreply.github.com>
2019-04-29 16:49:43 +0900

    Update practice2.py

--- 0402/practice2.py
+++ 0402/practice2.py
@@ -1,3 +1,5 @@
+#m이 n 의 약수인지 여부를 판별하는 함수isDivisor(m,n)
+
 def isDivisor(m,n):
     if m % n == 0:
         return True
@@ -8,4 +10,4 @@ def isDivisor(m,n):
 a = int(input('a = '))
 for i in range(1,a+1):
     if isDivisor(a,i):
-        print(i, end=' ')
+        print(i, end=' ')   #a의 모든 약수
