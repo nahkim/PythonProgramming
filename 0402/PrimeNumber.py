f574b2b2ed0a356ea97c7b1c1764cd58954bdf2b
hyunn2 <48798620+hyunn2@users.noreply.github.com>
2019-05-07 22:10:33 +0900

    Update PrimeNumber.py

--- 0402/PrimeNumber.py
+++ 0402/PrimeNumber.py
@@ -1,12 +1,17 @@
+# a까지의 소수출력
+
 a = int(input('- 입력 : '))
 
 while a < 2:
     print('2보다 큰 자연수 입력')
     a = int(input('- 입력 : '))
+    
 for k in range(2,a+1):
     s = 0
-    for i in range(1, int(k/2)+1):
+    isPrime = True
+    for i in range(2, int(k/2)+1):
         if k % i == 0:
-            s += i
-    if s == k:
-        print(k, end=' ')
+            isPrime = False
+
+    if isPrime:
+        print(k, end = ' ')
